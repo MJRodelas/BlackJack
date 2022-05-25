13e57caf1d41327797c095593c56bffbd3272f8f
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-25 19:15:40 +0800

    Start code of BlackJack
    Html and JS was created
    
    // Create two variables, firstCard and secondCard
    // Set their values to a random number between 2-11
    //Create a variable, sum and set it to the sum of the two cards

---
+++ index.js
@@ -0,0 +1,8 @@
+// Create two variables, firstCard and secondCard
+// Set their values to a random number between 2-11
+//Create a variable, sum and set it to the sum of the two cards
+
+let firstCard = 3
+let secondCard = 5
+let sum = firstCard + secondCard
+console.log(sum)
