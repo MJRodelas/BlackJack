402e870af7c820204dcf9920bc5225fd3f5e23a0
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-25 19:44:57 +0800

    Update index.js
    Adding Boolean hasBlackJack variable to detect if the user has BlackJack before cashout

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 // Set their values to a random number between 2-11
 //Create a variable, sum and set it to the sum of the two cards
 
-let firstCard = 3
-let secondCard = 5
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
 console.log(sum)
 
 if(sum < 21){
     console.log("Do you want to draw a new card?")
 }else if(sum === 21){
     console.log("You've a BlackJack!")
+    hasBlackJack = true
 }else{
     console.log("You're out of the game!")
 }
+
+//CashOut
+console.log(hasBlackJack)
