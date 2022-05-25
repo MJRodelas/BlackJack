8c605fff30aa6efeb40bf15f02eb4e77b52f3b18
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-25 19:22:17 +0800

    Update index.js
    Adding if else statements for the logic of the BlackJack Game

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ let firstCard = 3
 let secondCard = 5
 let sum = firstCard + secondCard
 console.log(sum)
+
+if(sum < 21){
+    console.log("Do you want to draw a new card?")
+}else if(sum === 21){
+    console.log("You've a BlackJack!")
+}else{
+    console.log("You're out of the game!")
+}
