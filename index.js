ab44779ad3f717a044b8f01f66f95bd156ff11e2
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-25 19:48:25 +0800

    Update index.js
    Adding Boolean isAlive variable to detect if player is still in the game

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
 console.log(sum)
+
+// 1. Create a variable called isAlive and assign it to true
+
+// 2. Flip its value to false in the appropriate code block 
 
 if(sum < 21){
     console.log("Do you want to draw a new card?")
-}else if(sum === 21){
+    }else if(sum === 21){
     console.log("You've a BlackJack!")
     hasBlackJack = true
 }else{
     console.log("You're out of the game!")
+    isAlive = false
 }
 
 //CashOut
 console.log(hasBlackJack)
+// 3. Log it out to check that you're doing it right
+console.log(isAlive)
