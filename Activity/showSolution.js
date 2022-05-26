08421815f860cd1b425e14f18c1de3386dd4e9bc
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-27 02:00:54 +0800

    Change = to ===

--- Activity/showSolution.js
+++ Activity/showSolution.js
@@ -4,7 +4,7 @@ let hasHintsLeft = false
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
-if (hasSolvedChallenge = false && hasHintsLeft = false) {
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
     showSolution()
 }
 
