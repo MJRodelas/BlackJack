72ceea691061f5b6791f80a1e460c8c096c4f429
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-27 02:53:59 +0800

    Create introToObjects.js
    //Objects - stote data in-depth - composite / complex data type
    // key-value pairs
    //always use dot notation

---
+++ Activity/introToObjects.js
@@ -0,0 +1,14 @@
+//Objects - stote data in-depth - composite / complex data type
+// key-value pairs
+
+let mySelf = {
+name: "Mark Jayson Rodelas",
+age: 26,
+gender: "Male",
+position:"Programmer",
+isOpenForHire: true,
+tags: ["HTML", "CSS","Javascript"]
+}
+
+console.log(mySelf.name)
+console.log(mySelf.tags)
