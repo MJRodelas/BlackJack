05076de3d2eb3aabff8a629d496ef175b6e5ad55
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-26 14:49:19 +0800

    Intro to arrays created
    Intro to use of arrays

---
+++ Activity/featuredPosts.js
@@ -0,0 +1,18 @@
+//Arays - ordered list of items
+
+let  myExperience = [
+    "Production Controller at Jollibee Foods Corporation",
+    "CS Rep at Sutherland Global Services",
+    "CS Rep at TaskUs"
+]
+
+let myEducation = [
+    "Santa Cruz North Central School",
+    "Dolores National High School",
+    "Guillermo D. Mendoza High School",
+    "Asian Institute of E-commerce",
+    "Mabalacat City College"
+]
+
+console.log(myExperience)
+console.log(myEducation)
