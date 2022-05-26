e662ea13756769d96aa3fe7c905275e83dfa1a6b
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-26 15:27:46 +0800

    Create countingInJs.js
    Counting in JS using for loop

---
+++ Activity/countingInJs.js
@@ -0,0 +1,22 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for ( let count = 10;  count < 15;  count += 1 )  {
+    
+    console.log(count)
+
+}
+
+//Create loop that counts from 10 to 100 steps of 10
+//Console.log to log out the numbers
+for ( let i = 10;  i < 101;  i += 10 )  {
+    
+    console.log(i)
+
+}
