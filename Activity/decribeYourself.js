45888226aa1ab5b56b3707d178efbac7b0141874
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-26 15:00:46 +0800

    Create decribeYourself.js
    //Make an array that has 3 data types that is about yourself
    //Name, Age, Do you like pizza?

---
+++ Activity/decribeYourself.js
@@ -0,0 +1,9 @@
+//Make an array that has 3 data types that is about yourself
+//Name, Age, Do you like pizza?
+let mySelf = [
+    "Mark Jayson D. Rodelas",
+    26,
+    true
+]
+
+console.log(mySelf)
