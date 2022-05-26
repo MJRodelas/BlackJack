569b28c794aa537af0a46cd9aed44b3961dfc070
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-27 01:53:27 +0800

    Use of logical operator &&
    //hasCompletedCourse === true && givesCertificate === true

---
+++ Activity/generateCertificate.js
@@ -0,0 +1,11 @@
+let hasCompletedCourse = true
+let givesCertificate = true
+
+//if both conditions satified, generate certificate
+if (hasCompletedCourse === true && givesCertificate === true) {
+       generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating certificate....")
+}
